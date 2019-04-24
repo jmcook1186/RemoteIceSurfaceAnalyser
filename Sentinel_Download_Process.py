11201c1b86d00e6b44f792db074686e5a158972f
Unknown <joe.cook@sheffield.ac.uk>
2019-04-24 21:38:45 +0100

    Tidy up printing to console and fix deleteL1C bug
    previous version was deleting L1C product too early,

--- Sentinel_Download_Process.py
+++ Sentinel_Download_Process.py
@@ -1,17 +1,12 @@
 """
 
 This function uses the sentinelsat python API to download batches of Sentinel 2 Level 1C images from SentinelHub,
-process them into L2A products using Sen2Cor and then save them to file. Then, a blob storage container is created
-with a name matching the tileID. The individual images are uploaded into the blob container and then removed from local
-storage. If a container already exists with name matching tileID, then the blobs are appended to the existing container.
+process them into L2A products using Sen2Cor and then save them to file.
 
-Since there is intermediate local storage (all dates in range for each tile) some preplanning is required to avoid
-running out of memory. For the ES20_v3 Linux VM with 160GB local storage, recommend setting a date range of up to 1 month
-at a time. The number of tiles is irrelevant because the local store is flushed of files before downloading images for
-each tile.
+NB Each product is 600MB zipped, up to 800MB when converted to L2A.Recommend downloading batches of ~20 tiles, sending
+to blob storage then deleting from hard drive, then repeating batch.
 
-Takes about 1 minute to download each tile on Azure ES20_v3 Linux DSVM, then up to 15 minutes to process L1Cto L2A.
-
+Takes about 1 minute to download each tile on Azure ES20_v3 Linux DSVM, then up to 15 minutes to process L1Cto L2A
 
 """
 
@@ -24,6 +19,7 @@ import os
 import shutil
 import fnmatch
 import glob
+
 
 
 def download_L1C(L1Cpath, tile, dates):
@@ -40,9 +36,9 @@ def download_L1C(L1Cpath, tile, dates):
 
     # define keyword arguments
     query_kwargs = {
-        'platformname': 'Sentinel-2',
-        'producttype': 'S2MSI1C',
-        'date': dates}
+            'platformname': 'Sentinel-2',
+            'producttype': 'S2MSI1C',
+            'date': dates}
 
     products = OrderedDict()
 
@@ -56,15 +52,16 @@ def download_L1C(L1Cpath, tile, dates):
     # keep metadata in pandas dataframe
     metadata = api.to_dataframe(products)
     L1Cfiles = metadata.filename
-    print('\n', ' DOWNLOADING {} FILES '.center(80, 'X').format(len(L1Cfiles)), '\n', 'X'.center(80, 'X'))
+    print('\n',' DOWNLOADING {} FILES '.center(80, 'X').format(len(L1Cfiles)),'\n', 'X'.center(80, 'X'))
 
     # download all files
-    api.download_all(products, directory_path=L1Cpath)
+    api.download_all(products, directory_path = L1Cpath)
+
 
     return L1Cfiles
 
 
-def process_L1C_to_L2A(L1C_path, L1Cfiles, unzip_files=True):
+def process_L1C_to_L2A(L1C_path, L1Cfiles, unzip_files = True):
     """
     This function is takes the downloaded L1C products from SentinelHub and converts them to L2A product using Sen2Cor.
     This is achieved as a batch job by iterating through the file names (L1Cfiles) stored in L1Cpath
@@ -78,17 +75,11 @@ def process_L1C_to_L2A(L1C_path, L1Cfiles, unzip_files=True):
         # unzip downloaded files in place, then delete zipped folders
         if unzip_files:
 
-            unzip = str('unzip ' + L1C_path + L1C[0:-4] + 'zip' + ' -d ' + L1C_path)
+            unzip = str('unzip '+ L1C_path + L1C[0:-4]+'zip'+' -d ' + L1C_path)
             os.system(unzip)
 
-            files = glob.glob(str(L1Cpath + '*.zip'))  # find files containing "L1C"
-            for f in files:
-                os.remove(f)
-
         # process L1C to L2A
-        sen2cor = str(
-            '/home/tothepoles/PycharmProjects/Sen2Cor-02.05.05-Linux64/bin/L2A_Process ' + L1C_path + '{}'.format(
-                L1C) + ' --resolution=20')
+        sen2cor = str('/home/tothepoles/PycharmProjects/Sen2Cor-02.05.05-Linux64/bin/L2A_Process ' + L1C_path + '{}'.format(L1C) + ' --resolution=20')
         os.system(sen2cor)
 
     return
@@ -103,18 +94,20 @@ def remove_L1C(L1Cpath):
 
     """
 
-    files = glob.glob(str(L1Cpath + '*L1C*'))  # find files containing "L1C"
+    files = glob.glob(str(L1Cpath+'*L1C*')) # find files containing "L1C"
 
     for f in files:
         try:
-            shutil.rmtree(f)  # try removing using shutil.rmtree() (for unzipped files)
+            shutil.rmtree(f) # try removing using shutil.rmtree() (for unzipped files)
         except:
-            os.remove(f)  # if it can't be removed by shutil then use os.remove()
+            os.remove(f) # if it can't be removed by shutil then use os.remove()
 
     return
 
 
+
 def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs=False):
+
     """
     Function uploads processed L2A products to blob storage. Check if container matching tile name already exists -if so
     add files to that container, if not, create new container with name = tile.
@@ -127,7 +120,7 @@ def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs
 
     block_blob_service = BlockBlobService(account_name=blob_account_name, account_key=blob_account_key)
 
-    container_name = tile.lower()  # convert string to lower case because Azure blobs named in lower case
+    container_name = tile.lower() #convert string to lower case because Azure blobs named in lower case
 
     local_path = L1Cpath
 
@@ -139,20 +132,21 @@ def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs
         filtered_paths = []
         filtered_names = []
 
-        folder_path = str(local_path + folder + "/")
+        folder_path = str(local_path+folder+"/")
 
         # append all file paths and names to list, then filter to the relevant jp2 files
         for (dirpath, dirnames, filenames) in os.walk(folder_path):
             file_paths += [os.path.join(dirpath, file) for file in filenames]
             file_names += [name for name in filenames]
 
-        for path in fnmatch.filter(file_paths, "*.jp2"):
-            filtered_paths.append(path)
+        for path in fnmatch.filter(file_paths,"*.jp2"):
+                filtered_paths.append(path)
 
-        for file in fnmatch.filter(file_names, "*.jp2"):
+        for file in fnmatch.filter(file_names,"*.jp2"):
             filtered_names.append(file)
 
-        # ' check for existing containers
+
+        #' check for existing containers
         existing_containers = block_blob_service.list_containers()
 
         existing_container_names = []
@@ -161,15 +155,15 @@ def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs
             existing_container_names.append(item.name)
 
         if any(tile.lower() in p for p in existing_container_names):
-            print('\n', ' CONTAINER {} ALREADY EXISTS IN STORAGE ACCOUNT '.center(80, 'X').format(tile),
+            print('\n',' CONTAINER {} ALREADY EXISTS IN STORAGE ACCOUNT '.center(80, 'X').format(tile),
                   '\n', 'X'.center(80, 'X'))
 
             # add files to existing container matching tile name
-            for i in np.arange(0, len(filtered_paths)):
-                print('\n', ' UPLOADING FOLDERS TO EXISTING CONTAINER {} '.center(80, 'X').format(tile),
+            for i in np.arange(0,len(filtered_paths)):
+                print('\n',' UPLOADING FOLDERS TO EXISTING CONTAINER {} '.center(80, 'X').format(tile),
                       '\n', 'X'.center(80, 'X'))
                 source = str(filtered_paths[i])
-                destination = str(folder + '/' + filtered_names[i])
+                destination = str(folder+'/' + filtered_names[i])
 
                 try:
                     block_blob_service.create_blob_from_path(container_name, destination, source)
@@ -214,8 +208,8 @@ def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs
             print('\n', ' CHECKING BLOBS FAILED '.center(80, 'X'),
                   '\n', 'X'.center(80, 'X'))
 
-        print('\n', ' BLOBS CURRENTLY STORED IN CONTAINER {}: '.center(80, 'X'))
-        print("\n", blob_list)
+        print('\n',' BLOBS CURRENTLY STORED IN CONTAINER {}: '.center(80, 'X'))
+        print("\n",blob_list)
 
         if any(folder in p for p in blob_list):
             print('\n', ' UPLOAD SUCCESSFUL: FOLDERS IN BLOB MATCH THOSE IN SCRIPT '.center(80, 'X'),
@@ -224,13 +218,13 @@ def send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs
         else:
             upload_status = 0
 
-    else:
-        upload_status = 1  # if check_blobs deselected, assume upload was successful (not recommended)
+    else: upload_status = 1 # if check_blobs deselected, assume upload was successful (not recommended)
 
     return upload_status
 
 
-def remove_L2A(L1Cpath, upload_status):  # delete unused L1C products
+
+def remove_L2A(L1Cpath, upload_status): # delete unused L1C products
 
     """
     Once files have been sent to blob, remove them from local storage
@@ -239,14 +233,14 @@ def remove_L2A(L1Cpath, upload_status):  # delete unused L1C products
     :return:
     """
 
-    files = glob.glob(str(L1Cpath + '*L2A*'))  # find files containing "L1C"
+    files = glob.glob(str(L1Cpath+'*L2A*')) # find files containing "L1C"
 
     if upload_status == 1:
         for f in files:
             try:
-                shutil.rmtree(f)  # try removing using shutil.rmtree() (for unzipped files)
+                shutil.rmtree(f) # try removing using shutil.rmtree() (for unzipped files)
             except:
-                os.remove(f)  # if it can't be removed by shutil it is likely zipped - use os.remove()
+                os.remove(f) # if it can't be removed by shutil it is likely zipped - use os.remove()
 
     else:
         print('\n', ' S2A FILES NOT UPLOADED TO BLOB: S2A FILES NOT DELETED FROM LOCAL STORAGE '.center(80, 'X'), '\n',
@@ -255,21 +249,23 @@ def remove_L2A(L1Cpath, upload_status):  # delete unused L1C products
     return
 
 
+
+
 ########### DEFINE VARIABLES ############
 #########################################
 
 
 # all tiles refers to all the individual tiles that cover the GRUS dark zone
-alltiles = ['22XDF', '21XWD', '21XWC', '22WEV', '22WEU', '22WEA', '22WEB', '22WED', '21XWB', '21XXA', '21WEC', '21XVD',
-            '22WES', '22VER', '22WET']
+alltiles = ['22XDF', '21XWD', '21XWC', '22WEV', '22WEU', '22WEA', '22WEB', '22WED', '21XWB', '21XXA', '21WEC', '21XVD', '22WES', '22VER', '22WET']
+
 
 ############ RUN FUNCTIONS #############
 ########################################
 
 # loop through tiles
-tiles = ['22WED', '21XWB', '21XXA', '21WEC', '21XVD', '22WES', '22VER', '22WET']
+tiles =  ['22WED', '21XWB']
 
-for i in np.arange(0, len(tiles), 1):
+for i in np.arange(0,len(tiles),1):
 
     # set start and end dates
     startDate = date(2017, 6, 1)
@@ -290,19 +286,19 @@ for i in np.arange(0, len(tiles), 1):
 
     tile = tiles[i]
 
-    print("CURRENT TILE_ID = ", tile)
+    print("\n CURRENT TILE_ID = ", tile)
 
     try:
         L1Cfiles = download_L1C(L1Cpath, tile, dates)
-        process_L1C_to_L2A(L1Cpath, L1Cfiles, unzip_files=True)
+        process_L1C_to_L2A(L1Cpath,L1Cfiles, unzip_files=True)
         remove_L1C(L1Cpath)
         upload_status = send_to_blob(blob_account_name, blob_account_key, tile, L1Cpath, check_blobs=True)
         remove_L2A(L1Cpath, upload_status)
 
-        print("TILE ID {} FINISHED".format(tile))
-        print(' MOVING TO NEXT TILE '.center(80, 'X'), '\n', 'X'.center(80, 'X'))
+        print("\n TILE ID {} FINISHED".format(tile))
+        print('\n MOVING TO NEXT TILE '.center(80, 'X'), '\n', 'X'.center(80, 'X'))
 
     except:
-        print("No images found for this tile on the specified dates")
+        print("\n No images found for this tile on the specified dates")
 
-print(' FINISHED ALL TILES '.center(80, 'X'), '\n', 'X'.center(80, 'X'))
+print('X'.center(80,'X'), ' FINISHED ALL TILES '.center(80,'X'),'\n','X'.center(80,'X'))
