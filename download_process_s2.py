1c1a08d053146c4e0ffe50b405c9a2a81df8b02e
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2019-07-31 15:09:20 +0100

    split sentinel2 functions into those for azure and those for local machine; refactor download script to use the new functions, works as a standalone driver script with options read from template file

---
+++ download_process_s2.py
@@ -0,0 +1,92 @@
+"""
+The user defines a list of relevant tiles and dates. This script then accesses the SentinelHub store and downloads the
+relevant tiles as zipped L1C products and saves them locally. The local file is then converted to the L2A product using
+the ESA Sen2Cor processor, overwriting the L1C product. The L2A product is then uploaded to blob storage and then erased
+from the local disk.
+
+In this script, two batches of dates are provided for each tile. This is to enable flushing of the local disk before too
+many tiles are saved. The files are large, and more than one month's worth of images can lead to the local disk filling
+up causing the script to crash. To avoid this, I iterate through month by month instead of providing a large date range.
+
+Blob storage is organised into separate containers for each tile with a separate folder for each date saved inside.
+
+tile
+|--date
+    |--individual band jp2s
+
+
+"""
+
+import numpy as np
+import os
+import sys
+import json
+import configparser
+import datetime as dt
+import calendar
+
+from sentinelsat import SentinelAPI
+
+import sentinel2_tools
+import sentinel2_azure
+
+# Get project configuration
+config = configparser.ConfigParser()
+config.read_file(open(sys.argv[1]))
+
+tiles = json.loads(config.get('options','tiles'))
+years = json.loads(config.get('options','years'))
+months = json.loads(config.get('options','months'))
+
+
+# Open API to Azure blob store
+azure_cred = configparser.ConfigParser()
+azure_cred.read_file(open(os.environ['AZURE_SECRET']))
+azure = sentinel2_azure.AzureAccess(azure_cred.get('account','user'),
+                                    azure_cred.get('account','key'))
+
+
+# Open API to Copernicus SciHub
+cscihub_cred = configparser.ConfigParser()
+cscihub_cred.read_file(open(os.environ['CSCIHUB_SECRET']))
+chub_api = SentinelAPI(cscihub_cred.get('account','user'), 
+        cscihub_cred.get('account','password'),
+        'https://scihub.copernicus.eu/apihub')
+
+
+# Iterate through tiles
+for tile in tiles:
+
+    # Download and pre-process one month at a time
+    for month in months:
+
+        # set start and end dates
+        startDate = dt.date(year, month, 1)
+        endDate = dt.date(year, month, calendar.monthrange(year,month)[1])
+
+        # dates creates a tuple from the user-defined start and end dates
+        dates = (startDate, endDate)
+
+        # set path to save downloads
+        L1Cpath = os.environ['PROCESS_DIR']
+
+        print("\n CURRENT TILE_ID = ", tile)
+
+        try:
+            L1Cfiles = sentinel2_tools.download_L1C(L1Cpath, tile, dates, 
+                config.get('thresholds','cloudCoverThresh'))
+            sentinel2_tools.process_L1C_to_L2A(L1Cpath, L1Cfiles, 
+                config.get('options','resolution'), unzip_files=True)
+            sentinel2_tools.remove_L1C(L1Cpath)
+            upload_status = azure.send_to_blob(tile, L1Cpath, check_blobs=True)
+            sentinel2_tools.remove_L2A(L1Cpath, upload_status)
+
+            print("\n TILE ID {} FINISHED".format(tile))
+            print('\n MOVING TO NEXT TILE '.center(80, 'X'), '\n', 'X'.center(80, 'X'))
+
+        # Should not catch all exceptions like this as this hides any genuine errors. Instead catch 
+        # specific errors only - not yet sure what these would be.
+        except: 
+            print("\n No images found for this tile on the specified dates")
+
+print('X'.center(80,'X'), ' FINISHED ALL TILES '.center(80,'X'),'\n','X'.center(80,'X'))
--- sentinel2_azure.py
+++ sentinel2_azure.py
@@ -17,6 +17,8 @@ import glob
 
 class AzureAccess:
 
+
+
     self._acc_name = None
     self._acc_key = None
     self.block_blob_service = None
@@ -33,99 +35,6 @@ class AzureAccess:
     def connect_to_service(self):
         self.block_blob_service = BlockBlobService(account_name=self._acc_name, 
             account_key=self._acc_key)
-
-
-
-    def download_L1C(L1Cpath, tile, dates, cloudcoverthreshold):
-        """
-        This function uses the sentinelsat API to download L1C products for tiles defined by "tile" in the date range
-        specified by "dates". Prints number of files and their names to console.
-
-        :param L1Cpath:
-        :param tile:
-        :param dates:
-
-        :return: L1Cfiles
-        """
-
-        # define keyword arguments
-        query_kwargs = {
-                'platformname': 'Sentinel-2',
-                'producttype': 'S2MSI1C',
-                'date': dates,
-                'cloudcoverpercentage': (0, cloudcoverthreshold)}
-
-        products = OrderedDict()
-
-        # loop through tiles and list all files in date range
-
-        kw = query_kwargs.copy()
-        kw['tileid'] = tile  # products after 2017-03-31
-        pp = api.query(**kw)
-        products.update(pp)
-
-        # keep metadata in pandas dataframe
-        metadata = api.to_dataframe(products)
-        L1Cfiles = metadata.filename
-        print('\n',' DOWNLOADING {} FILES '.center(80, 'X').format(len(L1Cfiles)),'\n', 'X'.center(80, 'X'))
-
-        # download all files
-        #api.download_all(products, directory_path = L1Cpath)
-
-
-        return L1Cfiles
-
-
-
-    def process_L1C_to_L2A(L1C_path, L1Cfiles, S2_resolution, unzip_files=True):
-        """
-        This function is takes the downloaded L1C products from SentinelHub and converts them to L2A product using Sen2Cor.
-        This is achieved as a batch job by iterating through the file names (L1Cfiles) stored in L1Cpath
-        Running this script will save the L2A products to the working directory
-
-        :return: None
-
-        """
-
-        sen2cor_bin = os.environ['SEN2COR_BIN']
-
-        for L1C in L1Cfiles:
-
-            # unzip downloaded files in place, then delete zipped folders
-            if unzip_files:
-
-                unzip = str('unzip '+ L1C_path + L1C[0:-4]+'zip'+' -d ' + L1C_path)
-                os.system(unzip)
-
-            # process L1C to L2A
-            try:
-                sen2cor = str(sen2cor_bin + '/L2A_Process ' + L1C_path + '{}'.format(L1C) + ' --resolution={}'.format(S2_resolution))
-                os.system(sen2cor)
-
-            except:
-                print("Processing {} L1C to L2A failed. Skipping to next tile".format(L1C))
-        return
-
-
-
-    def remove_L1C(L1Cpath):
-        """
-        function removes L1C files from wdir after processing to L2A
-
-        :param L1Cpath: path to wdir
-        :return:
-
-        """
-
-        files = glob.glob(str(L1Cpath+'*L1C*')) # find files containing "L1C"
-
-        for f in files:
-            try:
-                shutil.rmtree(f) # try removing using shutil.rmtree() (for unzipped files)
-            except:
-                os.remove(f) # if it can't be removed by shutil then use os.remove()
-
-        return
 
 
 
@@ -311,28 +220,3 @@ class AzureAccess:
 
         return filtered_bloblist, download_flag
 
-
-
-    def remove_L2A(L1Cpath, upload_status): # delete unused L1C products
-
-        """
-        Once files have been sent to blob, remove them from local storage
-
-        :param L1Cpath: path to wdir
-        :return:
-        """
-
-        files = glob.glob(str(L1Cpath+'*L2A*')) # find files containing "L1C"
-
-        if upload_status == 1:
-            for f in files:
-                try:
-                    shutil.rmtree(f) # try removing using shutil.rmtree() (for unzipped files)
-                except:
-                    os.remove(f) # if it can't be removed by shutil it is likely zipped - use os.remove()
-
-        else:
-            print('\n', ' S2A FILES NOT UPLOADED TO BLOB: S2A FILES NOT DELETED FROM LOCAL STORAGE '.center(80, 'X'), '\n',
-                  'X'.center(80, 'X'))
-
-        return
